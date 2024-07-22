a6bb3453fd7799e57e7412451c0c190461ad988f
Conor Huh <conor.huh@datashapes.com>
2024-07-22 13:30:08 -0500

    test setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 from version import __version__ as version
 
-
 setup(name='bq_helper',
+      packages=['bq_helper', 'test_helper'],
       version=version,
       description='Helper class to simplify common read-only BigQuery tasks.',
-      author='Sohier Dane',
-      url='https://github.com/SohierDane/BigQuery_Helper',
+      author='Conor Huh',
+      url='https://github.com/ConorHuh/BigQuery_Helper',
       license='Apache 2.0',
       install_requires=['pandas', 'google-cloud-bigquery'],
       classifiers=['Programming Language :: Python :: 3'],
